2cf12ce398e7149b383ae693534ccd5327b96496
Jakub Pelc <jakub.pelc@seznam.cz>
2023-03-25 14:58:37 +0100

    added simple board method with design outline

--- Catan/src/main/java/cz/cvut/fel/pjv/codenames/client/Board.java
+++ Catan/src/main/java/cz/cvut/fel/pjv/codenames/client/Board.java
@@ -1,4 +1,21 @@
 package cz.cvut.fel.pjv.codenames.client;
 
 public class Board {
+    private Card[] board;
+
+    public Card[] getBoard() {
+        return board;
+    }
+
+    public Board() {
+        //shuffle cards
+        //get 25 of them and make a board
+    }
+
+    private Card[] constructBoard(){
+        //shuffle deck
+        return new Card[25]; //get 25 top cards of deck
+    }
+
+
 }
