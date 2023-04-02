a3af09f74a4615db87cba177ec8a98680893efaf
Jakub Pelc <pelcjaku@fel.cvut.cz>
2023-04-02 14:49:25 +0200

    created java-fx project

---
+++ CodeNames/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cz.cvut.fel.pjv.codenames {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cz.cvut.fel.pjv.codenames to javafx.fxml;
+    exports cz.cvut.fel.pjv.codenames;
+}
