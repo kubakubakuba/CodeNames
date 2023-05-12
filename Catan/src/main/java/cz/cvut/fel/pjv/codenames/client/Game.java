695504a3086e4831be1ae3ad0842c2781dae5429
Jakub Pelc <pelcjaku@fel.cvut.cz>
2023-05-12 17:41:00 +0200

    removed old files

--- Catan/src/main/java/cz/cvut/fel/pjv/codenames/client/Game.java
+++
@@ -1,8 +0,0 @@
-package cz.cvut.fel.pjv.codenames.client;
-
-public class Game {
-    private Board board;
-    private Deck deck;
-    private Key key;
-    private Player[] players;
-}
--- Catan/src/main/java/cz/cvut/fel/pjv/codenames/server/Start.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fel.pjv.codenames.server;
-
-public class Start {
-    public static void main(String[] args) {
-        System.out.println("Starting server:");
-        Server server = new Server();
-        server.run();
-    }
-}
